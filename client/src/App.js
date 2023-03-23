dbfab032b4c482fb4d15c2b2b235be1ec53e044a
Wan Jong Kim <wkim2@9152.local>
2023-03-23 13:42:55 -0400

    Changed the axios default url to appropriate url

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import PlacePage from './pages/PlacePage';
 import BookingsPage from './pages/BookingsPage';
 import BookingPage from './pages/BookingPage';
 
-axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.baseURL = "https://booker-xqry.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
